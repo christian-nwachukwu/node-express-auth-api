0ff47b73322baf0338f068b2b7eff1404efa4429
Christian Nwachukwu <christian.nwachukwu2020@gmail.com>
2025-01-03 04:24:31 +0100

    Installed and imported dependencies, created the server configurations and file structures

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+//import dependencies
+import cookieParser from "cookie-parser";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import mongoose from "mongoose";
+//import route from "./routes/userRoute.js";
+
+//initialize express app
+const app = express();
+
+
+//parse json middleware to parse json request
+app.use(cookieParser());
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+    res.json({message:'Hello from the server'})
+})
+
+//load configurations and environmental variables
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MONGOURL = process.env.MONGO_URL;
+
+//connect to DB
+mongoose
+.connect(MONGOURL)
+.then(() => {
+    console.log("Database connected successfully.");
+    app.listen(PORT, ()=> {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+.catch((error) => console.log(error));
+
+//create api endpoint
+//app.use("/api/user", route);
